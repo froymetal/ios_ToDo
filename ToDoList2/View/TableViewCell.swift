a0ac82740fb5e4d1fd182f711a206c17fbac0909
Froy <froylan.almeida@gmail.com>
2021-06-22 14:09:01 -0500

    Un cambio

---
+++ ToDoList2/View/TableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewCell.swift
+//  ToDoList2
+//
+//  Created by Field Employee on 6/22/21.
+//
+
+import Foundation
+
+class AddNewTaskViewModel{
+
+}
--- ToDoList2/ViewController.swift
+++ ToDoList2/ViewController.swift
@@ -2,15 +2,14 @@
 //  ViewController.swift
 //  ToDoList2
 //
-//  Created by Field Employee on 6/22/21.
+//  Created by Froy on 6/22/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func toDoDate(_ sender: Any) {
-    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
