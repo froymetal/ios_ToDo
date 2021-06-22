5fce7edf3a7f098d0b423b3e20dde0e10c484def
Froy <froylan.almeida@gmail.com>
2021-06-22 15:26:42 -0500

    Commit Funcional 3

---
+++ ToDo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo+CoreDataClass.swift
+//  ToDoList2
+//
+//  Created by Field Employee on 6/22/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDo)
+public class ToDo: NSManagedObject {
+
+}
--- ToDoList2/ViewController.swift
+++ ToDoList2/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    //@IBOutlet var tableView: UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "To Do List"
     }
 
 
